6719c5218e5744e4517e33a76c9e9767f35c445f
tiburekouagou <tiburce.kouagou@3wa.io>
2022-01-30 15:27:47 +0100

    A dynamic clock build using html, css and vanilla js

---
+++ script.js
@@ -0,0 +1,22 @@
+const secondsHand = document.querySelector('[data-seconds]');
+const minutesHand = document.querySelector('[data-minutes]');
+const hoursHand = document.querySelector('[data-hours]');
+
+    setInterval(setClock, 1000);
+
+function setClock() {
+    let currentTime = new Date();
+    let secondsRatio = currentTime.getSeconds() / 60;
+    let minutesRatio =  (secondsRatio + currentTime.getMinutes()) / 60;
+    let hoursRatio = (minutesRatio + currentTime.getHours()) / 12 ;
+
+    setRotation(secondsHand, secondsRatio);
+    setRotation(minutesHand, minutesRatio);
+    setRotation(hoursHand, hoursRatio);
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock();
