b334355e1d9e7c00b56a8c6de7a23b42817ceeec
Conrad Renaud <recrad7@gmail.com>
2023-02-13 11:19:58 +0100

    MAP

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const secondsHand = document.querySelector('[data-seconds]');
-const minutesHand = document.querySelector('[data-minutes]');
-const hoursHand = document.querySelector('[data-hours]');
+const secondsHand = document.querySelector("[data-seconds]");
+const minutesHand = document.querySelector("[data-minutes]");
+const hoursHand = document.querySelector("[data-hours]");
 
-    setInterval(setClock, 1000);
+setInterval(setClock, 1000);
 
 function setClock() {
-    let currentTime = new Date();
-    let secondsRatio = currentTime.getSeconds() / 60;
-    let minutesRatio =  (secondsRatio + currentTime.getMinutes()) / 60;
-    let hoursRatio = (minutesRatio + currentTime.getHours()) / 12 ;
+  let currentTime = new Date();
+  let secondsRatio = currentTime.getSeconds() / 60;
+  let minutesRatio = (secondsRatio + currentTime.getMinutes()) / 60;
+  let hoursRatio = (minutesRatio + currentTime.getHours()) / 12;
 
-    setRotation(secondsHand, secondsRatio);
-    setRotation(minutesHand, minutesRatio);
-    setRotation(hoursHand, hoursRatio);
+  setRotation(secondsHand, secondsRatio);
+  setRotation(minutesHand, minutesRatio);
+  setRotation(hoursHand, hoursRatio);
 }
 
 function setRotation(element, rotationRatio) {
-    element.style.setProperty('--rotation', rotationRatio * 360)
+  element.style.setProperty("--rotation", rotationRatio * 360);
 }
 
 setClock();
